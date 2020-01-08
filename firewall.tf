c0572b640297e313fe82b906da0ba9aec58fdfde
JavierClairvaux <javcabr@gmail.com>
2020-01-08 13:22:26 -0800

    firewall config for vault and consul

---
+++ firewall.tf
@@ -0,0 +1,31 @@
+resource "google_compute_firewall" "vault_firewall" {
+	name    = "test-firewall"
+	network = google_compute_network.vault_vpc_network.name
+
+	allow {
+		protocol = "icmp"
+	}
+
+	// Consul ports
+	allow {
+		protocol = "tcp"
+		ports    = ["22", "8600", "8500", "8501", "8502", "8301", "8302", "8300", "21000-21255" ]
+	}
+
+	allow {
+		protocol = "udp"
+		ports    = ["8600",  "8301", "8302" ]
+	}
+
+
+	// Vault ports
+	allow {
+		protocol = "tcp"
+		ports    = ["7300", "7301", "8200", "8201" ]
+	}
+
+	allow {
+		protocol = "udp"
+		ports    = ["7301" ]
+	}
+}
