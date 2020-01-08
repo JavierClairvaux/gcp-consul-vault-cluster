752802807c62c86b754d32e410d328ac78ce36e7
JavierClairvaux <javcabr@gmail.com>
2020-01-08 13:24:55 -0800

    uncommented lines for first execution

--- gcp-vault-kms.tf
+++ gcp-vault-kms.tf
@@ -4,18 +4,18 @@ resource "google_service_account" "vault_kms_service_account" {
 }
 
 #Create a KMS key ring
-#resource "google_kms_key_ring" "vault_chatbot_keyring" {
-#   project  = var.gcp_project
-#   name     = var.key_ring
-#   location = var.key_ring_location
-# }
+resource "google_kms_key_ring" "vault_chatbot_keyring" {
+   project  = var.gcp_project
+   name     = var.key_ring
+   location = var.key_ring_location
+ }
 
 #Create a crypto key for the key ring
-#resource "google_kms_crypto_key" "chatbot_crypto_key" {
-#   name            = var.crypto_key
-#   key_ring        = google_kms_key_ring.vault_chatbot_keyring.self_link
-#   rotation_period = "100000s"
-# }
+resource "google_kms_crypto_key" "chatbot_crypto_key" {
+   name            = var.crypto_key
+   key_ring        = google_kms_key_ring.vault_chatbot_keyring.self_link
+   rotation_period = "100000s"
+ }
 
 # Add the service account to the Keyring
 resource "google_kms_key_ring_iam_binding" "vault_iam_kms_binding" {
